f368f3bb91f5bd0a0671fdebf49486180abf3bcc
Akhil Agrawal <akhil@Akhils-MacBook-Pro.local>
2021-01-16 22:29:09 +0530

    master changes

--- jfile.java
+++ jfile.java
@@ -4,7 +4,7 @@ public class jfile{
 
   public static void main(String args[]){
 
-    System.out.println("Akhil is amazing");
+    System.out.println("change in master");
     System.out.println("Akhil is too good");
 
   }
