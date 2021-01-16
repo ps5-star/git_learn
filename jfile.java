a3314a4c185fb47c171977d158ab3d004acb4e29
Akhil Agrawal <akhil@Akhils-MacBook-Pro.local>
2021-01-16 22:28:00 +0530

    master changes

--- jfile.java
+++ jfile.java
@@ -4,9 +4,8 @@ public class jfile{
 
   public static void main(String args[]){
 
-    System.out.println("This is my change to master");
+    System.out.println("Akhil is amazing");
     System.out.println("Akhil is too good");
-    System.out.println("This is my first branch");
 
   }
 
