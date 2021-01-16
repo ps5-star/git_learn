14ee87054bb269bff2748ed30356bfeb7098d207
Akhil Agrawal <akhil@Akhils-MacBook-Pro.local>
2021-01-16 22:28:43 +0530

    branch changes

--- jfile.java
+++ jfile.java
@@ -4,7 +4,7 @@ public class jfile{
 
   public static void main(String args[]){
 
-    System.out.println("Akhil is amazing");
+    System.out.println("Change in conflict branch");
     System.out.println("Akhil is too good");
 
   }
